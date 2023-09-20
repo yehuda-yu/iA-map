903f7a020897eb3a68d63d41129f40b6d72a5028
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-09-20 16:48:16 +0300

    Update main-iA-map.py

--- main-iA-map.py
+++ main-iA-map.py
@@ -53,10 +53,11 @@ if parameter in categorical_cols:
         lon='long',
         color=parameter,
         hover_data=[parameter, 'Village', 'District'],
-        color_discrete_map="Set1",  # Use a categorical color map
+        color_discrete_sequence=px.colors.qualitative.G10,
         zoom=8
     )
-    units = ""
+    # define units and threshold value
+    units = ''
     threshold_value = None
     pass
     
@@ -70,41 +71,34 @@ elif parameter in numerical_cols:
         color=parameter,
         size=parameter,
         hover_data=[parameter, 'Village', 'District'],
+        hover_name="Village",
         color_continuous_scale='Viridis',  # Replace with your desired color scale
         size_max=15,
         zoom=8
     )
-    units = st.sidebar.text_input("Enter Unit (e.g., mg/L, °C)", "mg/L")
+    # define units and threshold value by the dictionary
+    units = 'm'
     threshold_value = None
     pass
     
 elif parameter in threshold_cols:
 
-        # Use predefined dictionaries for threshold columns
-    threshold_dict = {
-        'Threshold_Column_1': 10.0,
-        'Threshold_Column_2': 20.0
-    }
-    color_dict = {
-        'Threshold_Column_1': {'Red': 'red', 'Green': 'green'},
-        'Threshold_Column_2': {'Red': 'red', 'Green': 'green'}
-    }
-    # Set threshold value and color dictionary based on the selected column
-    threshold_value = threshold_dict.get(parameter, 0.0)
-    color_map = color_dict.get(parameter, {'Red': 'red', 'Green': 'green'})
+    # Set threshold value and color dictionary based on the selected column and dictionary
+    threshold_value = float(units_and_thresholds[parameter][1][0])
     # Create the map for threshold columns
     fig = px.scatter_mapbox(
         data.dropna(subset=[parameter]),
         lat='lat',
         lon='long',
         color=parameter,
-        size=10,  # Set a default size for threshold columns
-        hover_data=[parameter, 'Village', 'District'],
-        color_discrete_map=color_map,
+        size=parameter,  # Set a default size for threshold columns
+        hover_data=[parameter, 'District'],
+        hover_name="Village",
+        color_discrete_map={'Red': 'red', 'Green': 'green'},
         size_max=15,
         zoom=8
     )
-    units = st.sidebar.text_input("Enter Unit (e.g., mg/L, °C)", "mg/L")
+    units = units_and_thresholds[parameter][0]
     pass
 
 # Set the minimum size of points
@@ -114,8 +108,9 @@ fig.update_traces(marker=dict(sizemin=5))
 if threshold_value is not None:
     fig.update_layout(legend_title_text=f"Threshold ({threshold_value} {units})")
 
-fig.update_layout(mapbox_style="carto-positron")
+fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
 
 # Show the map
 st.plotly_chart(fig)
