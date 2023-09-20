78030494290cc40b7f50c7509dbe30b4a79e7c88
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-09-20 14:00:42 +0300

    Update main-iA-map.py

--- main-iA-map.py
+++ main-iA-map.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Load your dataset
 @st.cache_data
@@ -27,7 +28,19 @@ data = load_data()
 
 # Sidebar for user input
 st.sidebar.header("Customize Visualization")
-parameter = st.sidebar.selectbox("Select a Parameter to Visualize", data.columns[2:])
+
+# define columns to choose from
+parameters_cols = ['Depth_of_overburden',
+       'Total_Depth', 'Depth_drilled_in_bedrock', 'BH Depth_1', 'BH Depth_2',
+       'BH Depth_3', 'BH Depth_4', 'Yield_L/s_1', 'Yield_L/s_2', 'Yield_L/s_3',
+       'Yield_L/s_4', 'Lithology_1', 'Lithology_2', 'Static_Water_Level',
+       'Stabilized_discharge(L/s)', 'Altitude_(m)', 'Date_Lab', 'Ph',
+       'Electrical Conductivity (EC)', 'Total dissolved solids', 'Turbidity',
+       'Colour', 'Alkalinity', 'Hardness', 'Chloride', 'Nitrate', 'Nitrite',
+       'Iron', 'Copper', 'Flouride', 'Sulphate', 'E.coli', 'Number',
+       'Suspended solids (total)', 'Manganese', 'Total Coliforms',]
+
+parameter = st.sidebar.selectbox("Select a Parameter to Visualize", parameters_cols)
 threshold_value = st.sidebar.text_input("Enter Threshold Value", data[parameter].median())
 units = st.sidebar.text_input("Enter Unit (e.g., mg/L, °C)", "mg/L")
 
@@ -66,8 +79,10 @@ fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 st.plotly_chart(fig)
 
 # Create a histogram below the map
-st.header(f"Distribution of {parameter}")
-st.plotly_chart(px.histogram(data, x=parameter, color='Color', nbins=20, opacity=0.7))
+fig_histogram = px.histogram(data, x=parameter, color='Color', nbins=20, opacity=0.7,
+                             color_discrete_map={'Red': 'red', 'Green': 'green'})
+fig_histogram.update_layout(title=f"Distribution of {parameter}")
+st.plotly_chart(fig_histogram)
 
 # Provide instructions to users
 st.sidebar.header("Instructions")
