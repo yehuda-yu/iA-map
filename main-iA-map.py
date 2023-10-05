73a69105e228163697558c2e465fe1d5ec4d266d
yehuda yungstein <81097407+yehuda-yu@users.noreply.github.com>
2023-10-05 17:11:01 +0300

    Update main-iA-map.py

--- main-iA-map.py
+++ main-iA-map.py
@@ -10,69 +10,83 @@ def load_data():
     csv_file = "gdf_84.csv"
     data = pd.read_csv(csv_file)
     
-    # Rename columns
-    column_rename_dict = {
-        "Depth of Overburden": "Overburden Thickness (m)",
-        "Depth Drilled in Bedrock (m)": "Depth Drilled in Bedrock (m)",
-        "Static Water Level (m)": "Static Water Level (m)",
-        "Stabilized Discharge": "Borehole Yield (L/s)",
-        "Altitude": "Elevation (m)",
+    # Convert relevant columns to numeric
+    numeric_columns = ['lat', 'long', 'Ph', 'Electrical Conductivity (EC)', 'Total dissolved solids',
+                       'Turbidity', 'Colour', 'Alkalinity', 'Hardness', 'Chloride', 'Nitrate', 'Nitrite',
+                       'Iron', 'Copper', 'Flouride', 'Sulphate', 'E.coli', 'Suspended solids (total)',
+                       'Manganese', 'Total Coliforms']
+    
+    data[numeric_columns] = data[numeric_columns].apply(pd.to_numeric, errors='coerce')
+
+    # replace 'BH Depth_1', 'BH Depth_2','BH Depth_3', 'BH Depth_4', 'Yield_L/s_1' names to 'First Water Strike Depth (m)'...
+    data = data.rename(columns={'BH Depth_1': 'First Water Strike Depth (m)', 'BH Depth_2': 'Second Water Strike Depth (m)', 'BH Depth_3': 'Third Water Strike Depth (m)', 'BH Depth_4': 'Fourth Water Strike Depth (m)', })
+    # replace Yield_L/s_1 to First Water Strike Yield (L/s)...
+    data = data.rename(columns={'Yield_L/s_1': 'First Water Strike Yield (L/s)', 'Yield_L/s_2': 'Second Water Strike Yield (L/s)', 'Yield_L/s_3': 'Third Water Strike Yield (L/s)', 'Yield_L/s_4': 'Fourth Water Strike Yield (L/s)','Stabilized_discharge(L/s)':'Borehole Yeild (L/s)' })
+
+        # Rename columns
+    column_mapping = {
+        "Depth_of_overburden": "Overburden Thickness (m)",
+        "Depth_drilled_in_bedrock": "Depth Drilled in Bedrock (m)",
+        "Static_Water_Level": "Static Water Level (m)",
+        "Stabilized_discharge(L/s)": "Borehole Yield (L/s)",
+        "Altitude_(m)": "Elevation (m)",
         "Ph": "pH",
         "Electrical Conductivity (EC)": "Electrical Conductivity (μS/cm)",
         "Total dissolved solids": "Total Dissolved Solids (mg/L)",
         "Turbidity": "Turbidity (NTU)",
-        "Colour": "Colour",
-        "Alkalinity": "Alkalinity (mg/L)",
         "Hardness": "Hardness (mg/L)",
         "Chloride": "Chloride (mg/L)",
-        "Nitrate as N": "Nitrate as N (mg/L)",
-        "Nitrite": "Nitrite (mg/L)",
-        "Iron": "Iron (mg/L)",
-        "Copper": "Copper (mg/L)",
-        "Flouride": "Flouride (mg/L)",
+        "Nitrate (as NO3-)": "Nitrate as N (mg/L)",
         "Sulphate": "Sulfate (mg/L)",
-        "E.coli": "E.coli",
-        "Suspended solids (total)": "Suspended solids (mg/L)",
-        "Manganese": "Manganese (mg/L)",
-        "Total Coliforms": "Total Coliforms",
     }
+    data.rename(columns=column_mapping, inplace=True)
     
-    data = data.rename(columns=column_rename_dict)
-    
-    # Drop columns to remove
-    columns_to_remove = ["Alkalinity (mg/L)", "E.coli", "Suspended solids (mg/L)", "Total Coliforms"]
-    data = data.drop(columns=columns_to_remove, errors='ignore')
-    
-    # Convert relevant columns to numeric
-    numeric_columns = ['lat', 'long', 'pH', 'Electrical Conductivity (μS/cm)', 'Total Dissolved Solids (mg/L)',
-                       'Turbidity (NTU)', 'Hardness (mg/L)', 'Chloride (mg/L)',
-                       'Nitrate as N (mg/L)', 'Nitrite (mg/L)', 'Iron (mg/L)', 'Copper (mg/L)',
-                       'Flouride (mg/L)', 'Sulfate (mg/L)', 'Suspended solids (mg/L)',
-                       'Manganese (mg/L)', 'Overburden Thickness (m)', 'Depth Drilled in Bedrock (m)',
-                       'Static Water Level (m)', 'Borehole Yield (L/s)', 'Elevation (m)']
-    
-    data[numeric_columns] = data[numeric_columns].apply(pd.to_numeric, errors='coerce')
+    # Drop columns
+    columns_to_drop = ["Colour", "Alkalinity", "Ecoli", "Suspended solids (total)"]
+    data.drop(columns=columns_to_drop, inplace=True)
     
     return data
 
 # Define the units and thresholds dictionary
 units_and_thresholds = {
-    "pH": ("", (9.5, 5.5)),
-    "Electrical Conductivity (μS/cm)": ("μS/cm", (2500,)),
-    "Total Dissolved Solids (mg/L)": ("mg/L", (1500,)),
-    "Turbidity (NTU)": ("NTU", (25,)),
-    "Hardness (mg/L)": ("mg/L", (600,)),
-    "Chloride (mg/L)": ("mg/L", (250,)),
-    "Copper (mg/L)": ("mg/L", (1,)),
-    "Flouride (mg/L)": ("mg/L", (1.5,)),
-    "Iron (mg/L)": ("mg/L", (0.3,)),
-    "Manganese (mg/L)": ("mg/L", (0.1,)),
-    "Nitrate as N (mg/L)": ("mg/L", (45,)),
-    "Sulfate (mg/L)": ("mg/L", (400,)),
+    "Ph": ("", (9.5, 5.5)),
+    "Electrical Conductivity (EC)": ("μS/cm", (2500,)),
+    "Total dissolved solids": ("mg/L", (1500,)),
+    "Colour": ("TCU", (50,)),
+    "Turbidity": ("NTU", (25,)),
+    "Hardness": ("mg/L", (600,)),
+    "Chloride": ("mg/L", (250,)),
+    "Cadmium (Cd)": ("mg/L", (0.003,)),
+    "Calcium (Ca)": ("mg/L", (150,)),
+    "Copper": ("mg/L", (1,)),
+    "Flouride": ("mg/L", (1.5,)),
+    "Iron": ("mg/L", (0.3,)),
+    "Manganese": ("mg/L", (0.1,)),
+    "Magneisum (Mg)": ("mg/L", (100,)),
+    "Nitrate (as NO3-)": ("mg/L", (45,)),
+    "Sodium (Na)": ("mg/L", (200,)),
+    "Chlorine Residue": ("mg/L", (0.2, 0.5)),
+    "Sulphate": ("mg/L", (400,)),
+    "Total Coliforms": ("CFU/100 ml", (0.0001,)),
+    "E.coli": ("CFU/100 ml", (0.0001,)),
+    "Aluminium (Al)": ("mg/L", (0.2,)),
+    "Arsenic (As)": ("mg/L", (0.01,)),
+    "Nitrate": ("mg/L", (10,)),
+    "Zinc (Zn)": ("mg/L", (3,)),
+    "Lead (Pb)": ("mg/L", (0.01,)),
+    "Mercury (Hg)": ("mg/L", (0.001,)),
+    "Cyanide": ("mg/L", (0.01,)),
+    "Selenium (Se)": ("mg/L", (0.01,)),
+    "Barium (Ba)": ("mg/L", (0.7,)),
+    "Ammonia (NH3)": ("mg/L", (0.5,)),
+    "Nickel (Ni)": ("mg/L", (0.02,)),
+    "Chromium (Cr)": ("mg/L", (0.05,)),
+    "Cobalt (Co)": ("mg/L", ("-")),
 }
 
 # App UI
 st.title("Water Drilling Points in Uganda")
+
 
 # Provide instructions to users
 st.sidebar.header("Instructions")
@@ -81,17 +95,20 @@ st.sidebar.markdown("Select a parameter to visualize from the dropdown menu.")
 # Load the data
 data = load_data()
 
+st.dataframe(data)
+
 # Sidebar for user input
 st.sidebar.header("Customize Visualization")
 
 # Define lists of columns
-categorical_cols = ['Village', 'District', 'Date_Completed', 'Lithology_1', 'Lithology_2']
-numerical_cols = ['Overburden Thickness (m)', 'Depth Drilled in Bedrock (m)', 'Static Water Level (m)',
-                   'Borehole Yield (L/s)', 'Elevation (m)']
-threshold_cols = ['pH', 'Electrical Conductivity (μS/cm)', 'Total Dissolved Solids (mg/L)',
-                   'Turbidity (NTU)', 'Hardness (mg/L)', 'Chloride (mg/L)', 'Nitrate as N (mg/L)',
-                   'Nitrite (mg/L)', 'Iron (mg/L)', 'Copper (mg/L)', 'Flouride (mg/L)', 'Sulfate (mg/L)',
-                   'Manganese (mg/L)']
+categorical_cols = ['Village','District','Date_Completed','First Water Strike Depth (m)', 'Second Water Strike Depth (m)',
+       'Third Water Strike Depth (m)', 'Fourth Water Strike Depth (m)', 'Lithology_1', 'Lithology_2']
+numerical_cols = ['Depth_of_overburden', 'Total_Depth', 'Depth_drilled_in_bedrock','First Water Strike Yield (L/s)', 'Second Water Strike Yield (L/s)',
+       'Third Water Strike Yield (L/s)', 'Fourth Water Strike Yield (L/s)', 'Static_Water_Level', 'Borehole Yeild (L/s)', 'Altitude_(m)',]
+threshold_cols = ['Ph', 'Electrical Conductivity (EC)', 'Total dissolved solids',
+                       'Turbidity', 'Colour', 'Alkalinity', 'Hardness', 'Chloride', 'Nitrate', 'Nitrite',
+                       'Iron', 'Copper', 'Flouride', 'Sulphate', 'E.coli', 'Suspended solids (total)',
+                       'Manganese', 'Total Coliforms']
 
 # Define all available columns in your dataset
 all_columns = categorical_cols + numerical_cols + threshold_cols
@@ -109,8 +126,7 @@ try:
             lat='lat',
             lon='long',
             color=parameter,
-            hover_data=[parameter, 'Village', 'Borehole Yield (L/s)', 'Nitrate as N (mg/L)',
-                        'Total Dissolved Solids (mg/L)', 'Elevation (m)'],
+            hover_data=[parameter, 'Village','Borehole Yeild (L/s)', 'Nitrate','Total dissolved solids', 'Altitude_(m)'],
             color_discrete_sequence=px.colors.qualitative.G10,
             zoom=8
         )
@@ -127,8 +143,7 @@ try:
             lon='long',
             color=parameter,
             size=parameter,
-            hover_data=[parameter, 'Village', 'Borehole Yield (L/s)', 'Nitrate as N (mg/L)',
-                        'Total Dissolved Solids (mg/L)', 'Elevation (m)'],
+            hover_data=[parameter, 'Village','Borehole Yeild (L/s)', 'Nitrate','Total dissolved solids', 'Altitude_(m)'],
             hover_name="Village",
             color_continuous_scale='plasma',  # Replace with your desired color scale
             size_max=15,
@@ -152,20 +167,19 @@ try:
             lon='long',
             color='Color',
             size=parameter,
-            hover_data=[parameter, 'Village', 'Borehole Yield (L/s)', 'Nitrate as N (mg/L)',
-                        'Total Dissolved Solids (mg/L)', 'Elevation (m)'],
+            hover_data=[parameter, 'Village','Borehole Yeild (L/s)', 'Nitrate','Total dissolved solids', 'Altitude_(m)'],
             color_discrete_map={'Red': 'red', 'Green': 'green'},
             size_max=15,
             zoom=8
         )
         units = units_and_thresholds[parameter][0]
 
-    elif parameter == 'pH':
+    elif parameter == 'Ph':
         # Get the lower and upper threshold values for pH
         lower_threshold, upper_threshold = units_and_thresholds[parameter][1]
     
         # Create a column 'Color' based on pH values
-        data['Color'] = data['pH'].apply(lambda x: 'Red' if x < lower_threshold or x > upper_threshold else 'Green')
+        data['Color'] = data['Ph'].apply(lambda x: 'Red' if x < lower_threshold or x > upper_threshold else 'Green')
     
         # Create the map for threshold columns
         fig = px.scatter_mapbox(
@@ -174,8 +188,7 @@ try:
             lon='long',
             color='Color',
             size=parameter,
-            hover_data=[parameter, 'Village', 'Borehole Yield (L/s)', 'Nitrate as N (mg/L)',
-                        'Total Dissolved Solids (mg/L)', 'Elevation (m)'],
+            hover_data=[parameter, 'Village', 'Borehole Yeild (L/s)', 'Nitrate', 'Total dissolved solids', 'Altitude_(m)'],
             color_discrete_map={'Red': 'red', 'Green': 'green'},
             size_max=15,
             zoom=8
